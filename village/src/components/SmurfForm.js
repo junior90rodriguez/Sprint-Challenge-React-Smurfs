c23f98df4db830a183ddc370fcfcee9d0ab57a09
Alberto Rodriguez <junior.arodriguez@icloud.com>
2019-06-30 20:25:27 -0500

    passed props from addSmurf to handleSubmit

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -10,9 +10,10 @@ class SmurfForm extends Component {
     };
   }
 
-  addSmurf = event => {
+  handleSubmit= event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    this.props.addSmurf(this.state.smurf)
 
     this.setState({
       name: '',
@@ -28,7 +29,7 @@ class SmurfForm extends Component {
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={this.handleSubmit}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
